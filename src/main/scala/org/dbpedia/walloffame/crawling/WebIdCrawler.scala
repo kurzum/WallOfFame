4dd1db4ccba4dd497808c4e9538ec3b487e4ef78
Eisenbahnplatte <18533014+Eisenbahnplatte@users.noreply.github.com>
2020-11-25 23:24:38 +0100

    updated init and execute permissions for generated crawl.sh

--- src/main/scala/org/dbpedia/walloffame/crawling/WebIdCrawler.scala
+++ src/main/scala/org/dbpedia/walloffame/crawling/WebIdCrawler.scala
@@ -6,22 +6,25 @@ import org.dbpedia.walloffame.validation.WebIdValidator.validate
 object WebIdCrawler {
 
   def crawl(): File = {
-    val crawlStream = getClass.getClassLoader.getResourceAsStream("/crawl.sh")
+    val crawlStream = getClass.getClassLoader.getResourceAsStream("crawl.sh")
     val in = scala.io.Source.fromInputStream(crawlStream)
 
+    println(crawlStream.available())
     val crawlFile = File("crawl.sh").toJava
 
     val out = new java.io.PrintWriter(crawlFile)
     try {
       in.getLines().foreach(out.println(_))
-      import sys.process._
-      val result: String = crawlFile.getAbsolutePath.!!
-
-      File(result.trim)
     }
     finally {
       out.close
-      crawlFile.delete()
     }
+
+    import sys.process._
+    "chmod +x ./crawl.sh".!!
+    val result: String = crawlFile.getAbsolutePath.!!
+
+    crawlFile.delete()
+    File(result.trim)
   }
 }
